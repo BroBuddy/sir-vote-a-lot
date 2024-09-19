0299b57c58c129cb91e6bbea5d255fa5bb7b446c
Bro Buddy <sirstone@gmx.de>
2024-09-19 14:25:38 +0200

    added zustand devtool

--- src/store/pollStore.ts
+++ src/store/pollStore.ts
@@ -1,4 +1,13 @@
 import { create } from 'zustand'
+import zustymiddlewarets from 'zustymiddlewarets'
+
+/* eslint-disable */
+declare global {
+    interface Window {
+        store: any
+    }
+}
+/* eslint-enable */
 
 type State = {
     question: string
@@ -18,70 +27,73 @@ const getCurrentTimestamp = (): string => {
     return String(new Date().getTime())
 }
 
-const usePollStore = create<State & Actions>((set, get) => ({
-    question: 'What is the value of π?',
-    options: [
-        { id: '1726240953842', text: '3.14', votes: 4 },
-        { id: '1726240953843', text: '3.1416', votes: 3 },
-        { id: '1726240953844', text: '3.14159264', votes: 4 },
-    ],
-    setQuestion: (question: string) => {
-        set(() => ({
-            question,
-        }))
-    },
-    setOptions: (options: Option[]) => {
-        set(() => ({
-            options,
-        }))
-    },
-    addOption: (optionText: string) => {
-        const options = get().options as Option[]
+const usePollStore = create<State & Actions>(
+    zustymiddlewarets((set, get) => ({
+        question: 'What is the value of π?',
+        options: [
+            { id: '1726240953842', text: '3.14', votes: 4 },
+            { id: '1726240953843', text: '3.1416', votes: 3 },
+            { id: '1726240953844', text: '3.14159264', votes: 4 },
+        ],
+        setQuestion: (question: string) => {
+            set(() => ({
+                question,
+            }))
+        },
+        setOptions: (options: Option[]) => {
+            set(() => ({
+                options,
+            }))
+        },
+        addOption: (optionText: string) => {
+            const options = get().options as Option[]
 
-        const newOption = {
-            id: getCurrentTimestamp(),
-            text: optionText,
-            votes: 0,
-        }
+            const newOption = {
+                id: getCurrentTimestamp(),
+                text: optionText,
+                votes: 0,
+            }
 
-        set(() => ({
-            options: [...options, newOption],
-        }))
-    },
-    removeOption: (optionText: string) => {
-        const newOptions = get().options as Option[]
-        const optionIndex = newOptions.findIndex(
-            (option) => option.text === optionText
-        )
+            set(() => ({
+                options: [...options, newOption],
+            }))
+        },
+        removeOption: (optionText: string) => {
+            const newOptions = get().options as Option[]
+            const optionIndex = newOptions.findIndex(
+                (option) => option.text === optionText
+            )
 
-        newOptions.splice(optionIndex, 1)
+            newOptions.splice(optionIndex, 1)
 
-        set(() => ({
-            options: [...newOptions],
-        }))
-    },
-    voteOption: (optionId: string) => {
-        const newOptions = get().options as Option[]
-        const optionIndex = newOptions.findIndex(
-            (option) => option.id === optionId
-        )
+            set(() => ({
+                options: [...newOptions],
+            }))
+        },
+        voteOption: (optionId: string) => {
+            const newOptions = get().options as Option[]
+            const optionIndex = newOptions.findIndex(
+                (option) => option.id === optionId
+            )
 
-        newOptions[optionIndex] = {
-            id: newOptions[optionIndex].id,
-            text: newOptions[optionIndex].text,
-            votes: newOptions[optionIndex].votes + 1,
-        }
+            newOptions[optionIndex] = {
+                id: newOptions[optionIndex].id,
+                text: newOptions[optionIndex].text,
+                votes: newOptions[optionIndex].votes + 1,
+            }
 
-        set(() => ({
-            options: [...newOptions],
-        }))
-    },
-    resetPoll: () => {
-        set(() => ({
-            question: '',
-            options: [],
-        }))
-    },
-}))
+            set(() => ({
+                options: [...newOptions],
+            }))
+        },
+        resetPoll: () => {
+            set(() => ({
+                question: '',
+                options: [],
+            }))
+        },
+    }))
+)
 
+window.store = usePollStore
 export default usePollStore
